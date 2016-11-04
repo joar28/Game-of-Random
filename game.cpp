7f17832b9836cad6a51a1254f8e1d81c0a8b5ccf
Joar A. Etterlid <joar28+git@gmail.com>
2016-11-04 16:21:29 +0100

    Initial Commit

---
+++ game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joar on 11/4/16.
+//
+
+#include "game.h"
---
+++ game.h
@@ -0,0 +1,14 @@
+//
+// Created by joar on 11/4/16.
+//
+
+#ifndef GAME_OF_RANDOM_GAME_H
+#define GAME_OF_RANDOM_GAME_H
+
+
+class game {
+
+};
+
+
+#endif //GAME_OF_RANDOM_GAME_H
