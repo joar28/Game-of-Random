8d2e1503edb1850182321f0353c453e37960feb2
Joar A. Etterlid <joar28+git@gmail.com>
2016-11-14 03:35:39 +0100

    deleted run_lint.sh

--- run_lint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-python styleguide/cpplint/cpplint.py --linelength=100 /media/hdd1/arkiv/tech/data/programmering/cpp/game_of_random/*.cpp /media/hdd1/arkiv/tech/data/programmering/cpp/game_of_random/*.h /media/hdd1/arkiv/tech/data/programmering/cpp/game_of_random/states/*
